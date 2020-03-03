76b942dd298852525ee79fce4610532b0e09a035
Victor Hugo Tello <victorhugotello@hotmail.com>
2020-03-02 21:55:07 -0700

    Manejo sencillo de base de datos

--- TiendaOnline/gestioPedidos/models.py
+++ TiendaOnline/gestioPedidos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class clientes(models.Model):
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    telefono = models.CharField(max_length=10)
+
+
+class articulos(models.Model):
+    nombre = models.CharField(max_length=30)
+    seccion = models.CharField(max_length=30)
+    precio = models.IntegerField()
+
+
+class pedidos(models.Model):
+    numero = models.IntegerField()
+    fecha = models.DateField()
+    entregado = models.BooleanField()
