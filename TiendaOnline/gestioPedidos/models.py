31045d9cbd66e434bb29f020df32db6783c2aa51
Victor Hugo Tello <victorhugotello@hotmail.com>
2020-03-03 21:35:15 -0700

    str pendiente de funcionar

--- TiendaOnline/gestioPedidos/models.py
+++ TiendaOnline/gestioPedidos/models.py
@@ -15,6 +15,10 @@ class articulos(models.Model):
     seccion = models.CharField(max_length=30)
     precio = models.IntegerField()
 
+    def __str__(self):
+        return 'El nombre es %s , la seccion'
+        ' es %s y el precio es %s' % (self.nombre, self.seccion, self.precio)
+
 
 class pedidos(models.Model):
     numero = models.IntegerField()
