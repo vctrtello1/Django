22830b14eefc6867ecb9ca7a0f4064c22763ff97
Victor Hugo Tello <victorhugotello@hotmail.com>
2020-03-05 20:22:07 -0700

    funcion __str__ funcionando

--- TiendaOnline/gestioPedidos/models.py
+++ TiendaOnline/gestioPedidos/models.py
@@ -16,8 +16,8 @@ class articulos(models.Model):
     precio = models.IntegerField()
 
     def __str__(self):
-        return 'El nombre es %s , la seccion'
-        ' es %s y el precio es %s' % (self.nombre, self.seccion, self.precio)
+        return 'El nombre es %s , la seccion es %s y el precio es %s' % (
+            self.nombre, self.seccion, self.precio)
 
 
 class pedidos(models.Model):
