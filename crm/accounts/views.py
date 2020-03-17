ca79f6c3d8444989511a9d4310805e0dc2dadc12
Victor Hugo Tello <victorhugotello@hotmail.com>
2020-03-16 17:01:27 -0700

    Basic Templates

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,3 +1,14 @@
-# from django.shortcuts import render
-
+from django.shortcuts import render
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def products(request):
+    return render(request, 'accounts/products.html')
+
+
+def costumer(request):
+    return render(request, 'accounts/costumer.html')
