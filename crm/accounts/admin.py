7ea06b6d40a097a511ac78d99111808bd8f275a6
Victor Hugo Tello <victorhugotello@hotmail.com>
2020-03-17 22:42:00 -0700

    First Migration

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Costumer, Products, Order
+
+admin.site.register(Costumer)
+admin.site.register(Products)
+admin.site.register(Order)
--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Costumer (models.Model):
+    name = models.CharField(max_length=60, null=True)
+    phone = models.CharField(max_length=60, null=True)
+    email = models.CharField(max_length=60, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Products (models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+    name = models.CharField(max_length=60, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=60, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=60, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order (models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    # costumer = models.CharField(max_length=60, null=True)
+    # product = models.CharField(max_length=60, null=True)
+    status = models.CharField(max_length=60, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
