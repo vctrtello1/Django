09261aeeccc528538f50300b9ef7a3e6c1b08020
Victor Hugo Tello <victorhugotello@hotmail.com>
2020-03-19 22:19:07 -0700

    Pending Tags

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
+from .models import Costumer, Products, Order
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    costumers = Costumer.objects.all()
+    total_costumers = costumers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {
+        'orders': orders, 'costumers': costumers,
+        'total_costumers': total_costumers, 'total_orders': total_orders,
+        'delivered': delivered, 'pending': pending
+        }
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Products.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 
 def costumer(request):
