3a01dbabd76ca6fbf822a4cd40ae620a01ce40f7
Victor Hugo Tello <victorhugotello@hotmail.com>
2020-03-19 20:55:50 -0700

    Foreign Key Migration

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Costumer, Products, Order
+from .models import Costumer, Products, Order, Tag
 
 admin.site.register(Costumer)
 admin.site.register(Products)
+admin.site.register(Tag)
 admin.site.register(Order)
--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -13,6 +13,13 @@ class Costumer (models.Model):
         return self.name
 
 
+class Tag (models.Model):
+    name = models.CharField(max_length=60, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Products (models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -21,8 +28,12 @@ class Products (models.Model):
     name = models.CharField(max_length=60, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=60, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=60, null=True)
+    description = models.CharField(max_length=60, null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Order (models.Model):
@@ -31,7 +42,9 @@ class Order (models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
-    # costumer = models.CharField(max_length=60, null=True)
-    # product = models.CharField(max_length=60, null=True)
+    costumer = models.ForeignKey(
+        Costumer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(
+        Products, null=True, on_delete=models.SET_NULL)
     status = models.CharField(max_length=60, null=True, choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
