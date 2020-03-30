253cf39670cad442a8dd81a42825e0cc07d409f8
Victor Hugo Tello <victorhugotello@hotmail.com>
2020-03-29 22:00:48 -0700

    Correct restore

--- crm/accounts/migrations/0010_auto_20200327_2051.py
+++ crm/accounts/migrations/0010_auto_20200327_2051.py
@@ -16,6 +16,9 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='costumer',
             name='user',
-            field=models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.OneToOneField(
+                blank=True, null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                to=settings.AUTH_USER_MODEL),
         ),
     ]
