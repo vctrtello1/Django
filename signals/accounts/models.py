32d5758c9ea827bc71e75d1ecba77dfb951f83ae
Victor Hugo Tello <victorhugotello@hotmail.com>
2020-04-01 22:08:16 -0700

    signal.py another method

--- signals/accounts/models.py
+++ signals/accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -14,21 +13,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
-
-
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-        print('Profile created')
-
-
-post_save.connect(create_profile, sender=User)
-
-
-def update_profile(sender, instance, created, **kwargs):
-    if created is False:
-        Profile.objects.create(user=instance)
-        print('Profie created for existing user')
-
-
-post_save.connect(update_profile, sender=User)
